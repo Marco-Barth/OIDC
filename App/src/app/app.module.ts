411c4358152f205832f8761888d095075205a5c7
mbart <mbarth509@gmail.com>
2024-01-07 17:16:26 +0100

    Create simple routing

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './component/main/main.component';
+import {Routes} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
