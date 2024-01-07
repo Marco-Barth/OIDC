867fa1f2721787d5a2674bbf69ace48376105ba9
mbart <mbarth509@gmail.com>
2024-01-07 16:22:57 +0100

    Create first backend packages & frontend Module (Angular)

---
+++ src/main/java/com/example/backend/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+}
